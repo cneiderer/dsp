8efa4b857c1b8d6b58b4e7b3182357189740e8c4
cneiderer <curtis.neiderer@gmail.com>
2017-11-06 08:34:13 -0800

    Completed Q6 functions

--- python/q6_strings.py
+++ python/q6_strings.py
@@ -1,6 +1,34 @@
 # Based on materials copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 
+def main():
+    print("\nTesting donuts():")
+    print("=================")
+    test_donuts()
+
+    print("\nTesting both_ends():")
+    print("====================")
+    test_both_ends()
+
+    print("\nTesting fix_start():")
+    print("====================")
+    test_fix_start()
+
+    print("\nTesting mix_up():")
+    print("===================")
+    test_mix_up()
+
+    print("\nTesting verbing():")
+    print("==================")
+    test_verbing()
+
+    print("\nTesting not_bad:")
+    print("================")
+    test_not_bad()
+
+    print("\nTesting front_back():")
+    print("=====================")
+    test_front_back()
 
 def donuts(count):
     """
@@ -18,7 +46,17 @@ def donuts(count):
     >>> donuts(99)
     'Number of donuts: many'
     """
-    raise NotImplementedError
+
+    if count > 9:
+        count = 'many'
+
+    return "Number of donuts: {}".format(count)
+
+
+def test_donuts():
+    counts = [4, 9, 10, 99]
+    for count in counts:
+        print("donuts({0}) => {1}".format(count, donuts(count)))
 
 
 def both_ends(s):
@@ -37,7 +75,17 @@ def both_ends(s):
     >>> both_ends('xyz')
     'xyyz'
     """
-    raise NotImplementedError
+    
+    if len(s) >= 2:
+        return s[:2] + s[-2:]
+    else:
+        return ''
+
+
+def test_both_ends():
+    strings = ['spring', 'Hello', '', 'xyz']
+    for s in strings:
+        print("both_ends({0}) => {1}".format(s, both_ends(s)))
 
 
 def fix_start(s):
@@ -56,7 +104,21 @@ def fix_start(s):
     >>> fix_start('donut')
     'donut'
     """
-    raise NotImplementedError
+    
+    s_new = s[0]
+    for char in s[1:]:
+        if char is s[0]:
+            s_new += '*' 
+        else:
+            s_new += char
+
+    return s_new
+
+
+def test_fix_start():
+    strings = ['babble', 'aardvark', 'google', 'donut']
+    for s in strings:
+        print("fix_start({0}) => {1}".format(s, fix_start(s)))
 
 
 def mix_up(a, b):
@@ -74,7 +136,17 @@ def mix_up(a, b):
     >>> mix_up('pezzy', 'firm')
     'fizzy perm'
     """
-    raise NotImplementedError
+    
+    new_a = b[:2] + a[2:]
+    new_b = a[:2] + b[2:]
+
+    return "{0} {1}".format(new_a, new_b)
+
+
+def test_mix_up():
+    strings = [('mix', 'pod'), ('dog', 'dinner'), ('gnash', 'sport'), ('pezzy', 'firm')]
+    for s in strings:
+        print("mix_up({0}, {1}) => {2}".format(s[0], s[1], mix_up(s[0], s[1])))
 
 
 def verbing(s):
@@ -91,7 +163,21 @@ def verbing(s):
     >>> verbing('do')
     'do'
     """
-    raise NotImplementedError
+    
+    if len(s) > 2:
+        last3 = s[-3:]
+        if last3 == 'ing':
+            s = s + 'ly'
+        else:
+            s = s + 'ing'
+
+    return s
+
+
+def test_verbing():
+    strings = ['hail', 'swimming', 'do']
+    for s in strings:
+        print("verbing({0}) => {1}".format(s, verbing(s)))           
 
 
 def not_bad(s):
@@ -111,7 +197,34 @@ def not_bad(s):
     >>> not_bad("It's bad yet not")
     "It's bad yet not"
     """
-    raise NotImplementedError
+
+    import string
+
+    end_char = ''
+    if s[-1] in string.punctuation:
+        end_char = s[-1]
+        s = s[:-1]
+    words = s.split()
+
+    try:
+        not_idx = words.index('not')
+        bad_idx = words.index('bad')
+    except:
+        return s
+
+    if not_idx < bad_idx:
+        return " ".join(words[:not_idx] + ['good'] + words[bad_idx+1:]) + end_char
+    else:
+        return s
+
+
+def test_not_bad():
+    strings = ['This movie is not so bad', 
+               'This dinner is not that bad!', 
+               'This tea is not hot', 
+               "It's bad yet not"]
+    for s in strings:
+        print("not_bad({0}) => {1}".format(s, not_bad(s)))
 
 
 def front_back(a, b):
@@ -130,4 +243,20 @@ def front_back(a, b):
     >>> front_back('Kitten', 'Donut')
     'KitDontenut'
     """
-    raise NotImplementedError
+
+    from math import ceil    
+
+    a_div_pt = ceil(len(a) / 2)
+    b_div_pt = ceil(len(b) / 2)
+
+    return a[:a_div_pt] + b[:b_div_pt] + a[a_div_pt:] + b[b_div_pt:]
+    
+
+def test_front_back():
+    strings = [('abcd', 'xy'), ('abcde', 'xyz'), ('Kitten', 'Donut')]
+    for s in strings:
+        print("front_back({0}, {1}) => {2}".format(s[0], s[1], front_back(s[0], s[1])))    
+
+
+if __name__ == "__main__":
+    main()
